e81e938f7363a379f660988a7dd579ba6879e692
srivastavas07 <72646476+srivastavas07@users.noreply.github.com>
2024-07-23 02:28:45 +0530

    upp

--- backend/index.js
+++ backend/index.js
@@ -30,9 +30,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/user",userRoutes);
 app.use("/api/v1/tweet",tweetRoutes);
-// app.get("/",(res,req)=>{
-//     req.send("Server is running");
-// });
+app.get("/",(res,req)=>{
+    req.send("Server is running");
+});
 
 app.get('/favicon.ico', (req, res) => (
     res.status(200).sendFile('favicon.ico', {root: __dirname + '/static/'})
