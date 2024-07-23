58392036fe2c53f4975c130bd2a314fbd4d51d19
srivastavas07 <72646476+srivastavas07@users.noreply.github.com>
2024-07-23 17:18:50 +0530

    debugging both ok

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const USER_END_POINT = "http://localhost:8080/api/v1/user";
-export const TWEET_END_POINT = "http://localhost:8080/api/v1/tweet";
+export const USER_END_POINT = "https://kwitter-backend.vercel.app/api/v1/user";
+export const TWEET_END_POINT = "https://kwitter-backend.vercel.app/api/v1/tweet";
