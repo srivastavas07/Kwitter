751b0e8f397bc93eaa0bd38f14eae14a03f4f6f4
srivastavas07 <72646476+srivastavas07@users.noreply.github.com>
2024-07-26 15:35:02 +0530

    netlify added

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({extended:true}));
 // cors basically tells the backend the origin from where the request is coming. to avoid any unauthorised access.
 
 const corsOption = {
-    origin: ["https://kwitter07.vercel.app"],
+    origin: ["https://kwitter07.netlify.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
